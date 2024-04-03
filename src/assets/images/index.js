114bd12e8d7f691335534f2e0b8639cce6986f64
Jordy <jordycorporan95@gmail.com>
2024-04-03 09:27:01 -0400

    Add Corousel Feature /n Use Swiper library to integrate a corousel, added pagitation and navigation base on Swiper documentation.Need to further enhance images and encapsulate possible image component

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -27,6 +27,24 @@ import training from './training.jpg'
 import venmo from './venmo.jpg'
 import young_player from './young_player.jpg'
 import youth_team from './youth_team.jpg'
+import g1 from './G1.jpg'
+import g2 from './G2.jpg'
+import g3 from './G3.jpg'
+import g4 from './G4.jpg'
+import g5 from './G5.jpg'
+import g6 from './G6.jpg'
+import g7 from './G7.jpg'
+import g8 from './G8.jpg'
+import g9 from './G9.jpg'
+import g10 from './G10.jpg'
+import g11 from './G11.jpg'
+import g12 from './G12.jpg'
+import g13 from './G13.jpg'
+import g14 from './G14.jpg'
+import g15 from './G15.jpg'
+import g16 from './G16.jpg'
+import g17 from './G17.jpg'
+import g18 from './G18.jpg'
 
 
 export {
@@ -59,4 +77,22 @@ export {
     footerLogo,
     headerLogo,
     hero1,
-}
+    g1,
+    g2,
+    g3,
+    g4,
+    g5,
+    g6,
+    g7,
+    g8,
+    g9,
+    g10,
+    g11,
+    g12,
+    g13,
+    g14,
+    g15,
+    g16,
+    g17,
+    g18
+};
--- src/sections/Gallery.jsx
+++ src/sections/Gallery.jsx
@@ -1,12 +1,85 @@
 import React from 'react'
-
+import { Swiper, SwiperSlide } from 'swiper/react'
+import {Navigation, Pagination } from 'swiper/modules'
+import {
+  g1,
+  g2,
+  g3,
+  g4,
+  g5,
+  g6,
+  g7,
+  g8,
+  g9,
+  g10,
+  g11,
+  g12,
+  g13,
+} from '../assets/images'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 const Gallery = () => {
-    return (
-        <div className='leading text-2xl'>
-            Will Be Gallery
-            <h1 className='p-4 text-bold text-red-600 text-2xl'>Teams and Players maybe 15 and do a carousel </h1>
-        </div>
-    )
+  return (
+    <>
+      <h1 className='text-3xl font-bold mb-10'>Gallery </h1>
+      <Swiper
+        modules={[Pagination, Navigation]}
+        loop={true}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        spaceBetween={30}
+        slidesPerView={4}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <img src={g1} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g2} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g3} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g4} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g4} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g5} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g6} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g7} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g8} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g9} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g10} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g11} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g12} alt='Gallery Image' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={g13} alt='Gallery Image' />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  )
 }
 
 export default Gallery
