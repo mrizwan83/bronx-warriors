938f0e09c4ef030c030772d2075d9c8ef2525878
Jordy <jordycorporan95@gmail.com>
2024-04-04 11:08:48 -0400

    Added direction linkon top of map

--- src/sections/Map.jsx
+++ src/sections/Map.jsx
@@ -1,15 +1,24 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
-
 const Map = () => {
   return (
     <>
       <h1 className='text-5xl font-bold text-center mb-10'>Our Location</h1>
+      <div>
+        <a
+          href={`https://www.google.com/maps/dir/?api=1&destination=${40.86701504484206},${-73.81013174794911}`}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <button className='bg-red-400 rounded-md p-1'>Get directions</button>
+        </a>
+      </div>
+
       <MapContainer
         center={[40.86701504484206, -73.81013174794911]}
         zoom={13}
-        className='h-96'
+        className='h-96 rounded-sm'
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -17,9 +26,7 @@ const Map = () => {
         />
 
         <Marker position={[40.86701504484206, -73.81013174794911]}>
-          <Popup>
-            Pelham Bay Park Football Field
-          </Popup>
+          <Popup>Pelham Bay Park Football Field</Popup>
         </Marker>
       </MapContainer>
     </>
