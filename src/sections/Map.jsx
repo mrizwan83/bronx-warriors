62b7d59d0a3ce2a75ae1a6205740e0c99fb419cc
Jordy <jordycorporan95@gmail.com>
2024-04-04 11:01:22 -0400

    Add basic lied asic marker that is included in the library

--- src/sections/Map.jsx
+++ src/sections/Map.jsx
@@ -1,11 +1,11 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import MapMarker from '../assets//icons/marker-icon.png'
+
 
 const Map = () => {
   return (
     <>
-        <h1 className='text-5xl font-bold text-center mb-10'>Our Location</h1>
+      <h1 className='text-5xl font-bold text-center mb-10'>Our Location</h1>
       <MapContainer
         center={[40.86701504484206, -73.81013174794911]}
         zoom={13}
@@ -15,6 +15,12 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
+
+        <Marker position={[40.86701504484206, -73.81013174794911]}>
+          <Popup>
+            Pelham Bay Park Football Field
+          </Popup>
+        </Marker>
       </MapContainer>
     </>
   )
